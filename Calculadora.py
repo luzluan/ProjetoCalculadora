e46913f1e6c46d98c2d36aa1684d6de29b1a7abe
Luan Luz <luandonato@protonmail.com>
2024-12-14 23:59:36 -0300

    Primeiro passo

--- Calculadora.py
+++ Calculadora.py
@@ -1,8 +1,13 @@
-print('Hello World')
-
+def menu():
+    print("Escolha o tipo que operação que deseja fazer:")
+    print("1.Adição")
+    print("2.Subtração")
+    print("3.Multiplicação")
+    print("4.Divisão")
+    print("5.Cancelar")
+'''
+def calculadora()
+    while True
+'''
 #teste
-
-a=1
-b=2
-
-print(a+b)
+print('Hello World')
