4cf944248f3b1f93f484994bd29f4b57bfd970f1
Luan Luz <luandonato@protonmail.com>
2024-12-19 01:39:10 -0300

    Janela

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,12 @@
+import Tkinter as tk
+
+janela = tk.Tk()
+janela.title("Calculadora")
+janela.geometry(300x400+200+100)
+janela.config(bg="white")
+janela.mainloop()
+
+
 def menu():
     print("Escolha o tipo que operação que deseja fazer:")
     print("1.Adição")
@@ -5,9 +14,9 @@ def menu():
     print("3.Multiplicação")
     print("4.Divisão")
     print("5.Cancelar")
-'''
+
 def calculadora()
     while True
-'''
-#teste
+
+
 print('Hello World')
